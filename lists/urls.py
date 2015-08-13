a695c19dfd5e1ce7252bd20bca6b755cf775a772
Gary Herd <garyherd@yahoo.com>
2015-08-12 23:48:01 -0500

    refactor away some duplication in urls.py

---
+++ lists/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+# noinspection PyUnresolvedReferences
+from lists import views
+
+urlpatterns = [
+    # Examples:
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+    # url(r'^admin/', include(admin.site.urls)),
+]
