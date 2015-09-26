0c4fa243354b9b35331e72cfc2155ffdf870fabf
Gary Herd <garyherd@yahoo.com>
2015-09-26 14:05:27 -0500

    update view to use new existing list form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,10 @@ from django.template.loader import render_to_string
 from django.utils.html import escape
 from ..views import home_page
 from ..models import Item, List
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
+from ..forms import (
+    ItemForm, EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR, ExistingListItemForm
+)
 from unittest import skip
 
 class HomePageTest(TestCase):
@@ -90,13 +93,12 @@ class ListViewTest(TestCase):
 
     def test_form_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -104,7 +106,7 @@ class ListViewTest(TestCase):
             '/lists/%d/' % (list1.id,),
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
@@ -112,7 +114,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 
